fb8705afd05e1627dd29144596b026026079264f
Reece Webb <inbox@reece.org>
2015-02-16 16:37:40 -0800

    Wrote a custom view controller

--- Calcohol/ViewController.m
+++ Calcohol/ViewController.m
@@ -8,20 +8,226 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
 
-@property (weak, nonatomic) IBOutlet UITextField *beerPercentTextField;
-@property (weak, nonatomic) IBOutlet UISlider *beerCountSlider;
-@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
-@property (weak, nonatomic) IBOutlet UILabel *beerCountLabel;
+@property (weak, nonatomic) UIButton *calculateButton;
+@property (weak, nonatomic) UITapGestureRecognizer *hideKeyboardTapGestureRecognizer;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)loadView {
+    // Allocate and initialize the all-encompassing view
+    self.view = [[UIView alloc] init];
+    
+    // Allocate and initialize each of our views and the gesture recognizer
+    UITextField *textField = [[UITextField alloc] init];
+    UISlider *slider = [[UISlider alloc] init];
+    UILabel *label = [[UILabel alloc] init];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] init];
+    
+    // Add each view and the gesture recognizer as the view's subviews
+    [self.view addSubview:textField];
+    [self.view addSubview:slider];
+    [self.view addSubview:label];
+    [self.view addSubview:button];
+    [self.view addGestureRecognizer:tap];
+    
+    // textfield constraints
+    // width
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:textField
+                                                          attribute:NSLayoutAttributeWidth
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeWidth
+                                                         multiplier:0.8
+                                                           constant:0.0]];
+    // height
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:textField
+                                                          attribute:NSLayoutAttributeHeight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeHeight
+                                                         multiplier:0.0
+                                                           constant:44.0]];
+    // center horizontally
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:textField
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    // center vertically
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:textField
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:0.5
+                                                           constant:0.0]];
+    
+    // slider constraints
+    // width
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:slider
+                                                          attribute:NSLayoutAttributeWidth
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:textField
+                                                          attribute:NSLayoutAttributeWidth
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    // height
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:slider
+                                                          attribute:NSLayoutAttributeHeight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:textField
+                                                          attribute:NSLayoutAttributeHeight
+                                                         multiplier:0.0
+                                                           constant:44.0]];
+    // center horizontally
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:slider
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:textField
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    // center vertically
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:slider
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:textField
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.0
+                                                           constant:50.0]];
+    
+    // label constraints
+    // width
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:label
+                                                          attribute:NSLayoutAttributeWidth
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:slider
+                                                          attribute:NSLayoutAttributeWidth
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    // height
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:label
+                                                          attribute:NSLayoutAttributeHeight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:slider
+                                                          attribute:NSLayoutAttributeHeight
+                                                         multiplier:0.0
+                                                           constant:200.0]];
+    // center horizontally
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:label
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:slider
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    // center vertically
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:label
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:slider
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.0
+                                                           constant:50.0]];
+    
+    // button constraints
+    // width
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:button
+                                                          attribute:NSLayoutAttributeWidth
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:label
+                                                          attribute:NSLayoutAttributeWidth
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    // height
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:button
+                                                          attribute:NSLayoutAttributeHeight
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:label
+                                                          attribute:NSLayoutAttributeHeight
+                                                         multiplier:0.0
+                                                           constant:44.0]];
+    // center horizontally
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:button
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:label
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    // center vertically
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:button
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:label
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.0
+                                                           constant:100.0]];
+    
+    // Assign the views and gesture recognizer to our properties
+    self.beerPercentTextField = textField;
+    self.beerCountSlider = slider;
+    self.resultLabel = label;
+    self.calculateButton = button;
+    self.hideKeyboardTapGestureRecognizer = tap;
+    
+    self.beerPercentTextField.translatesAutoresizingMaskIntoConstraints = NO;
+    self.beerCountSlider.translatesAutoresizingMaskIntoConstraints = NO;
+    self.resultLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    self.calculateButton.translatesAutoresizingMaskIntoConstraints = NO;
+    
+}
+
+- (void)viewDidLoad
+{
+    // Calls the superclass's implementation
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Set our primary view's background color to lightGrayColor
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    // Tells the text field that `self`, this instance of `BLCViewController` should be treated as the text field's delegate
+    self.beerPercentTextField.delegate = self;
+    
+    // Set the placeholder text
+    self.beerPercentTextField.placeholder = NSLocalizedString(@"% Alcohol Content Per Beer", @"Beer percent placeholder text");
+    
+    // Set the text field border
+    self.beerPercentTextField.borderStyle = UITextBorderStyleRoundedRect;
+    
+    // Tells `self.beerCountSlider` that when its value changes, it should call `[self -sliderValueDidChange:]`.
+    // This is equivalent to connecting the IBAction in our previous checkpoint
+    [self.beerCountSlider addTarget:self action:@selector(sliderValueDidChange:) forControlEvents:UIControlEventValueChanged];
+    
+    // Set the minimum and maximum number of beers
+    self.beerCountSlider.minimumValue = 1;
+    self.beerCountSlider.maximumValue = 10;
+    
+    // Tells `self.calculateButton` that when a finger is lifted from the button while still inside its bounds, to call `[self -buttonPressed:]`
+    [self.calculateButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Set button options
+    self.calculateButton.titleLabel.font = [UIFont boldSystemFontOfSize:16];
+    self.calculateButton.backgroundColor = [UIColor lightGrayColor];
+    [self.calculateButton setTitle:NSLocalizedString(@"Calculate!", @"Calculate command") forState:UIControlStateNormal];
+    [self.calculateButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    
+    // Tells the tap gesture recognizer to call `[self -tapGestureDidFire:]` when it detects a tap.
+    [self.hideKeyboardTapGestureRecognizer addTarget:self action:@selector(tapGestureDidFire:)];
+    
+    // Gets rid of the maximum number of lines on the label
+    self.resultLabel.numberOfLines = 0;
+}
+
+- (void) viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,7 +235,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)textFieldDidChange:(UITextField *)sender {
+- (void)textFieldDidChange:(UITextField *)sender {
     // Make sure the text is a number
     NSString *enteredText = sender.text;
     float enteredNumber = [enteredText floatValue];
@@ -40,16 +246,11 @@
     }
 }
 
-- (IBAction)sliderValueDidChange:(UISlider *)sender {
+- (void)sliderValueDidChange:(UISlider *)sender {
     NSLog(@"Slider value changed to %f", sender.value);
     [self.beerPercentTextField resignFirstResponder];
     
-    // Show the slider value
-    
-    self.beerCountLabel.text = [NSString stringWithFormat:@"%0.0f", self.beerCountSlider.value];
-    
     // Show the results as we slide
-    
     // First, calculate how much alcohol is in all those beers...
     
     int numberOfBeers = self.beerCountSlider.value;
@@ -91,13 +292,13 @@
     self.resultLabel.text = resultText;
 }
 
-- (IBAction)buttonPressed:(UIButton *)sender {
+- (void)buttonPressed:(UIButton *)sender {
     [self.beerPercentTextField resignFirstResponder];
     
     // Code moved to sliderValueDidChange{} for label auto-updating as we slide
 }
 
-- (IBAction)tapGestureDidFire:(UITapGestureRecognizer *)sender {
+- (void)tapGestureDidFire:(UITapGestureRecognizer *)sender {
     [self.beerPercentTextField resignFirstResponder];
 }
 
---
+++ Calcohol/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Calcohol
+//
+//  Created by Reece Webb on 2/16/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
